494ac09deedb492443a075c8063c796bf726173d
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-11-24 12:33:12 +0500

    update

--- Lab03.cpp
+++ Lab03.cpp
@@ -6,20 +6,20 @@ int power(int b, int e){
 	int result = b;
 	if (e == 0)
 		return 1;
-		
+
 	for (int i=1; i<e; i++)
 		result *= b;
-		
+
 	return result;
 }
 
 string Max(string s1, string s2, string s3){
 	if (s1.length() > s2.length() && s1.length() > s3.length())
 		return s1;
-	
+
 	else if (s2.length() > s1.length() && s2.length() > s3.length())
 		return s2;
-		
+
 	else
 		return s3;
 }
@@ -33,7 +33,7 @@ int numlength(int x){
 	int length = 1;
 	while(x > 9)
 		x = x / 10, length++;
-	
+
 	return length;
 }
 
@@ -42,20 +42,20 @@ char maxChar(string str){
 	for (int i=1; i<str.length(); i++)
 		if (str[i] > max)
 			max = str[i];
-			
+
 	return max;
 }
 
 void print(int a[], int size){
 	for (int i=0; i<size; i++)
 		cout << "Value " << i+1 << " is : " << a[i] << endl;
-	
+
 }
 
 void reversePrint(int a[], int size){
 	for (size; size>0; size--)
 		cout << "Value " << size << " is : " << a[size-1] << endl;
-	
+
 }
 
 void input(int a[], int size){
@@ -63,7 +63,7 @@ void input(int a[], int size){
 		cout << "Enter value " << i+1 << " : ";
 		cin >> a[i];
 
-	}				
+	}
 	print(a, size);
 	cout << endl;
 	reversePrint(a, size);
@@ -75,11 +75,11 @@ int main(void){
 //	cout << Max("My name is Faizan Ahmad", "Hello World!", "What is your name?") << endl;
 //	cout << Max("Hello", "Hell0", "Hell1") << endl;
 //	range(0, 21);
-//	cout << numlength(2345) << endl; 
+//	cout << numlength(2345) << endl;
 //	cout << maxChar("Hamza");
 //	cout << maxChar("Do i know you?");
 	int arr[5];
-	input(arr, 5);	
+	input(arr, 5);
 
 	return 0;
 }
